c1e60146978de78e2c60b87a7ae5e8c316e2b2fe
Viktoria <vika.shakun@gmail.com>
2020-04-25 10:08:42 +0300

    Basic frontend changes using bootstrap

--- todo_site/main/urls.py
+++ todo_site/main/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path("<int:id>", views.index, name = "index")
+    path("<int:id>", views.index, name = "index"),
+    path("", views.home, name="home"),
+    path("create/", views.create, name="create")
+
 ]
